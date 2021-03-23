45f66dbc44a3f4930336b27f30d5754887f07e2c
Huggins Mafigu <hugginssd@users.noreply.github.com>
2021-03-23 22:40:30 +0200

    TestController

---
+++ Data/DbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiModel.Data
+{
+    public class DbContext : IdentityDbContext
+    {
+        public DbContext(DbContextOptions<DbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
