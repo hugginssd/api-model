c94e82a345f07f882a5a2af5d8b776bebbb6ae64
Huggins Mafigu <hugginssd@users.noreply.github.com>
2021-04-01 00:48:29 +0200

    Creating an API SDK with refit

--- ApiModel.Contracts/V1/Requests/UserRegistrationRequest.cs
+++ ApiModel.Contracts/V1/Requests/UserRegistrationRequest.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +7,6 @@ namespace ApiModel.Contracts.V1.Requests
 {
     public class UserRegistrationRequest
     {
-        [EmailAddress]
         public string Email { get; set; }
         public string Password { get; set; }    
     }
