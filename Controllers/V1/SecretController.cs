af413f72887af3eca9afb024c099d0f3661fce7a
Huggins Mafigu <hugginssd@users.noreply.github.com>
2021-04-01 11:45:11 +0200

    Setting up apikey authentication

---
+++ Controllers/V1/SecretController.cs
@@ -0,0 +1,19 @@
+﻿using ApiModel.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiModel.Controllers.V1
+{
+    [ApiKeyAuth]
+    public class SecretController : ControllerBase
+    {
+        [HttpGet("secret")]
+        public IActionResult GetSecret()
+        {
+            return Ok("I have no secret");
+        }
+    }
+}
